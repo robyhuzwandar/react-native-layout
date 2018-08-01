7b34ac1db2cdce1be5afb4d05d47895386f0c923
robyhuzwandar <robyhuzwandar@gmail.com>
2018-08-01 17:59:59 +0800

    first commit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      dateText: 'Wed Aug 01 2018',
       titleText: "Bird's Nest",
       bodyText: 'This is not a bird nest This is not really a bird nest This is not really a bird nest This is not really a bird nest.'
     };
@@ -15,7 +16,7 @@ export default class App extends React.Component {
       Alert.alert("You tapped the button!");
     }
     return <View style={styles.container}>
-        <Text style={styles.dateText} > {Date()} </Text>
+        <Text style={styles.dateText} > {this.state.dateText} </Text>
         <Text style={styles.titleText} > {this.state.titleText} </Text>
 
         <View style={{ height: 150, backgroundColor: "silver", marginBottom: 15, flexDirection:'row' , padding:10}}>
@@ -24,6 +25,9 @@ export default class App extends React.Component {
             </Text>
             <Image style={{ width: 125, height: 130, alignItems:'flex-end', flex:5 }} source={{ uri: "https://facebook.github.io/react-native/docs/assets/favicon.png" }} />
         </View>
+
+        <Text style={styles.dateText} > {this.state.dateText} </Text>
+        <Text style={styles.titleText} > {this.state.titleText} </Text>
 
        <View style={{ height: 150, backgroundColor: "silver", marginBottom: 15, flexDirection:'row' , padding:10}}>
             <Text style={{flex:7}}>
@@ -42,13 +46,18 @@ const styles = StyleSheet.create({
     marginTop: 35,
     flexDirection: 'column',
     padding: 10,
-    paddingTop: 15
+    paddingTop: 15,
     // justifyContent: 'center',
-    // alignItems: 'stretch',
+    alignItems: 'flex-start'
   },
 
   titleText: {
     fontSize: 20,
     fontWeight: 'bold',
   },
+
+  dateText:{
+    alignSelf: 'flex-end',
+    alignItems:'flex-end'
+  }
 });

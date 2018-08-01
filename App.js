68594c2195de7e5e244831c5767e2c2aee39a1c8
robyhuzwandar <robyhuzwandar@gmail.com>
2018-08-01 19:26:54 +0800

    first commit

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert, Image } from "react-native";
+import ActionButton from "react-native-action-button";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -16,26 +18,33 @@ export default class App extends React.Component {
       Alert.alert("You tapped the button!");
     }
     return <View style={styles.container}>
-        <Text style={styles.dateText} > {this.state.dateText} </Text>
-        <Text style={styles.titleText} > {this.state.titleText} </Text>
+        <Text style={styles.dateText}> {this.state.dateText} </Text>
+        <Text style={styles.titleText}> {this.state.titleText} </Text>
 
-        <View style={{ height: 150, backgroundColor: "silver", marginBottom: 15, flexDirection:'row' , padding:10}}>
-            <Text style={{flex:7}}>
-                {this.state.bodyText}
-            </Text>
-            <Image style={{ width: 125, height: 130, alignItems:'flex-end', flex:5 }} source={{ uri: "https://facebook.github.io/react-native/docs/assets/favicon.png" }} />
+        <View style={{ height: 150, backgroundColor: "silver", marginBottom: 15, flexDirection: "row", padding: 10 }}>
+          <Text style={{ flex: 7 }}>{this.state.bodyText}</Text>
+          <Image style={{ width: 125, height: 130, alignItems: "flex-end", flex: 5 }} source={{ uri: "https://facebook.github.io/react-native/docs/assets/favicon.png" }} />
         </View>
 
-        <Text style={styles.dateText} > {this.state.dateText} </Text>
-        <Text style={styles.titleText} > {this.state.titleText} </Text>
+        <Text style={styles.dateText}> {this.state.dateText} </Text>
+        <Text style={styles.titleText}> {this.state.titleText} </Text>
 
-       <View style={{ height: 150, backgroundColor: "silver", marginBottom: 15, flexDirection:'row' , padding:10}}>
-            <Text style={{flex:7}}>
-                {this.state.bodyText}
-            </Text>
-            <Image style={{ width: 125, height: 130, alignItems:'flex-end', flex:5 }} source={{ uri: "https://facebook.github.io/react-native/docs/assets/favicon.png" }} />
+        <View style={{ height: 150, backgroundColor: "silver", marginBottom: 15, flexDirection: "row", padding: 10 }}>
+          <Text style={{ flex: 7 }}>{this.state.bodyText}</Text>
+          <Image style={{ width: 125, height: 130, alignItems: "flex-end", flex: 5 }} source={{ uri: "https://facebook.github.io/react-native/docs/assets/favicon.png" }} />
         </View>
 
+        <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor="#9b59b6" title="New Task" onPress={() => console.log("notes tapped!")}>
+            <Icon name="md-create" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor="#3498db" title="Notifications" onPress={() => {}}>
+            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor="#1abc9c" title="All Tasks" onPress={() => {}}>
+            <Icon name="md-done-all" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
       </View>;
   }
 }

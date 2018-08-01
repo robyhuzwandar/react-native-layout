41e82fdd2102a9d189287dfd3bcfe387e150fb05
robyhuzwandar <robyhuzwandar@gmail.com>
2018-08-01 16:55:47 +0800

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import {Home} from './src/app/Home'
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
 
 export default class App extends React.Component {
 
   
   render() {
-    
+    const handlePress = () => {
+      Alert.alert("You tapped the button!");
+    }
     return (
-        <Home/>
+      
+      <View style={styles.container}>
+        {/* <Button
+          containerStyle={{ padding: 10, height: 45, overflow: 'hidden', borderRadius: 4, backgroundColor: 'white' }}
+          title="Add"
+          color="#841584"
+          onPress={handlePress}
+          accessibilityLabel="Learn more about this purple button"
+        /> */}
+        
+        <View style={{ height: 150, backgroundColor: 'silver', marginBottom:15, flexDirection: 'row'}}>
+          <View style={{ width: 150, backgroundColor: 'black', }}>
+
+          </View>
+        </View>
+        <View style={{ height: 150, backgroundColor: 'silver' }} />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 35,
+    flexDirection: 'column',
+    padding: 10,
+    paddingTop: 15
+    // justifyContent: 'center',
+    // alignItems: 'stretch',
+  },
+});
